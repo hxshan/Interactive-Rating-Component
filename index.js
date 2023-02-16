41cc3bb712f14e4c679675fad3a3d26ee6c10499
hxshan <galgodageheshan@gmail.com>
2023-02-16 22:39:20 +0530

    fix

--- index.js
+++ index.js
@@ -8,15 +8,17 @@ let ratings = document.querySelectorAll(".btn")
 ratings.forEach(function(rate){
     rate.addEventListener('click',function(){
         sel_rate.innerText=rate.innerText
-        console.log(rate)
     })
     
 })
 
 
 submit_btn.addEventListener('click',function(){
-    rating_div.style.display="none"
-    thank_div.style.display="flex"
+    rating_div.classList.remove("rating-container");
+    rating_div.classList.add("hidden");
+
+    thank_div.classList.remove("hidden");
+    thank_div.classList.add("thank-section");
 
 })
 
